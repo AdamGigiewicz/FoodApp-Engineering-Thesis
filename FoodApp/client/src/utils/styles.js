1a3177ee3f0f6181eb6e75ebb0ade98efabf4353
AdamGigiewicz <adamgigiewicz05@gmail.com>
2024-01-04 00:49:54 +0100

    feat: Admin Dashboard - Category in New Item

--- FoodApp/client/src/utils/styles.js
+++ FoodApp/client/src/utils/styles.js
@@ -3,3 +3,10 @@ export const isActiveStyles =
 
 export const isNotActiveStyles =
 "text-xl text-textColor  hover:text-red-700 px-4 py-2 duration-100 transition-all ease-in-out";
+
+export const statuses = [
+    {id: 1 , title: "Napoje", category: "napoje"},
+    {id: 2 , title: "Zupy", category: "zupy"},
+    {id: 3 , title: "Dania główne", category: "dania główne"},
+    {id: 4 , title: "Desery", category: "desery"}
+];
