dc9a4794b47fd7f9827b926ee82f0b896c7b95fe
AdamGigiewicz <adamgigiewicz05@gmail.com>
2024-01-19 00:21:34 +0100

    feat: Admin Dashboard - Integrating Data with Datable

--- FoodApp/client/src/components/DashboardItems.jsx
+++ FoodApp/client/src/components/DashboardItems.jsx
@@ -1,10 +1,63 @@
 import React from 'react';
-import {DataTable} from '../components';
+import { DataTable } from '../components';
+import { useSelector } from 'react-redux';
+import { HiMiniCurrencyDollar } from 'react-icons/hi2';
 
 const DashboardNewItem = () => {
-    return (
-    <div className="flex items-center justify-center gap-4 pt-6 w-full">
-      <DataTable/>
+  const products = useSelector(state => state.products);
+
+  return (
+    <div className="w-full">
+      <DataTable
+        columns={[
+          {
+            title: "Zdjęcie",
+            field: "imageURL",
+            render: (rowData) => (
+              <img
+                src={rowData.imageURL}
+                className="w-32 h-16 object-contain rounded-md"
+              />
+            ),
+          },
+          {
+            title: "Nazwa",
+            field: "product_name",
+          },
+          {
+            title: "Kategoria",
+            field: "product_category",
+          },
+          {
+            title: "Cena",
+            field: "product_price",
+            render: (rowData) => (
+              <p className="text-xl font-semibold text-textColor flex items-center justify-center">
+                <HiMiniCurrencyDollar className="text-red-400" />
+                {parseFloat(rowData.product_price).toFixed(2)}
+              </p>
+            ),
+          },
+        ]}
+        data={products}
+        title="Lista produktów"
+        actions={[
+          {
+            icon: "edit",
+            tooltip: "Edytuj",
+            onclick: (event, rowData) => {
+              alert("Chcesz edytować " + rowData.productId)
+            },
+          },
+          {
+            icon: "delete",
+            tooltip: "Usuń",
+            onclick: (event, rowData) => {
+              alert("Chcesz usunąć " + rowData.productId)
+            },
+          },
+        ]}
+      />
     </div>
   );
 };
